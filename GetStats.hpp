b6084b94c2bad0948623e0d21a57c7be53ccd3f3
smruthip07 <smruthi7777@gmail.com>
2025-03-28 02:59:00 +0000

    lassst

--- GetStats.hpp
+++ GetStats.hpp
@@ -16,23 +16,23 @@ public:
     int* getStats() const{
         //write your function here
     if(head != nullptr){
-        int* min = new int(head->data);
-        int* max = new int(head->data);
-        int* avg = new int(0);
+        int min = head->data;
+        int max = head->data;
+        int avg = 0;
         
         Node* temp = head;
 
         while(temp != nullptr){
-            if(temp->data < *min){
-                *min = temp->data;
+            if(temp->data < min){
+                min = temp->data;
             }
             temp = temp->next;
         }
         temp = head;
 
         while(temp != nullptr){
-            if(temp->data > *max){
-                *max = temp->data;
+            if(temp->data > max){
+                max = temp->data;
             }
             temp = temp->next;
         }
@@ -40,16 +40,18 @@ public:
 
         int c = 0;
         while(temp != nullptr){
-            *avg += temp->data;
+            avg += temp->data;
             temp = temp->next;
             c++;
         }
-        *avg = *avg/c;
-        
-        int* arr[3] = {min, max, avg};
-        delete min;
-        delete max;
-        delete avg;
+        avg = avg/c;
+        int* min1 = new int(min);
+        int* max1 = new int(max);
+        int* avg1 = new int(avg);
+        int* arr[3] = {min1, max1, avg1};
+        delete min1;
+        delete max1;
+        delete avg1;
         return arr[0];
     }
         return nullptr;
